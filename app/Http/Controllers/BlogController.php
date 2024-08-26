5f415292e9e917ea5b6176687fd0d76a55aa2be4
Guilherme Augusto dos Santos Viana <guilherme.gasv@policiacientifica.sp.gov.br>
2024-08-26 20:35:20 -0300

    adicionando navbar

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    public function index(){
-        return view('BLog.index');
+    public function index()
+    {
+        return view('blog.index'); // Certifique-se de que 'blog.index' corresponde ao caminho da view
     }
 }
